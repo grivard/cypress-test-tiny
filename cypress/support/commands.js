df06cf2804bb295736bccd96ba6424b28a7c9b26
grivard <58826572+grivard@users.noreply.github.com>
2020-04-20 16:53:48 -0400

    Add files via upload

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,39 +1,64 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create the custom command: 'login'.
-//
-// The commands.js file is a great place to
-// modify existing commands and create custom
-// commands for use throughout your tests.
-//
-// You can read more about custom commands here:
-// https://on.cypress.io/api/commands
-// ***********************************************
-//
-// Cypress.Commands.add("login", function(email, password){
-//   var email    = email || "joe@example.com"
-//   var password = password || "foobar"
-//
-//   var log = Cypress.Log.command({
-//     name: "login",
-//     message: [email, password],
-//     consoleProps: function(){
-//       return {
-//         email: email,
-//         password: password
-//       }
-//     }
-//   })
-//
-//   cy
-//     .visit("/login", {log: false})
-//     .contains("Log In", {log: false})
-//     .get("#email", {log: false}).type(email, {log: false})
-//     .get("#password", {log: false}).type(password, {log: false})
-//     .get("button", {log: false}).click({log: false}) //this should submit the form
-//     .get("h1", {log: false}).contains("Dashboard", {log: false}) //we should be on the dashboard now
-//     .url({log: false}).should("match", /dashboard/, {log: false})
-//     .then(function(){
-//       log.snapshot().end()
-//     })
-// })
+
+Cypress.Commands.add('GetTestEnvironment', () => {
+    cy.visit('/')
+    cy.get('#footer').then(($footer) => {
+		cy.log($footer.text()) // logs correctly if line 31 is false; doesn't log at all if line 31 is true
+/* This is an example of what the footer text should look like:
+
+	EOC
+	6.5.22959
+	|
+	culture
+	en-GB
+	|
+	uiCulture
+	en-GB
+	|
+	Request Server Time
+	20/04/2020 18:59 UTC+01:00
+
+*/
+		
+        var eoc = ParseFooter($footer.text())
+        cy.wrap(eoc)
+		cy.log(eoc)
+    })
+})
+
+function ParseFooter (footertext) {
+	cy.log(footertext) // logs correctly if line 31 is false; doesn't log at all if line 31 is true
+
+	if(true) { // comment this if block in/out to manifest failure
+		
+		var eocversion
+		var uiculture
+		var culture
+		var timewithoffset
+		var footerlines = footertext.split('\n')  // this line is failing with Cannot read property 'split' of undefined
+		var k = footerlines.keys();
+		for (let x of k) {
+			if (footerlines[x].indexOf('EOC') > 0) {
+				eocversion = footerlines[x+1].trim()
+			}
+			if (footerlines[x].indexOf('culture') > 0) {
+				culture = footerlines[x+1].trim()
+			}
+			if (footerlines[x].indexOf('uiCulture') > 0) {
+				uiculture = footerlines[x+1].trim()
+			}
+			if (footerlines[x].indexOf('Request Server Time') > 0) {
+				timewithoffset = footerlines[x+1].trim()
+			}
+		}
+
+		var timesplit = timewithoffset.split('UTC')
+
+		return {
+			"version": eocversion,
+			"culture": culture,
+			"uiculture": uiculture,
+			"time": timesplit[0].trim(),
+			"offset": timesplit[1]
+		}
+	}
+}
